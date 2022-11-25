3642efcecc25d46bb967d2ff142e4c137bd0cb69
Jan Bloniarz <01171621@pw.edu.pl>
2022-11-25 18:20:09 +0100

    Materialy z warsztatow IoT

---
+++ iot_warsztaty/pomiar/pomiar.ino
@@ -0,0 +1,16 @@
+double napiecie=0;
+double odczytanawartosc=0;
+void setup() {
+  pinMode(A1,INPUT);
+  Serial.begin(9600);
+  analogReference(INTERNAL);
+}
+
+void loop() {
+  odczytanawartosc=analogRead(A1);
+  napiecie=odczytanawartosc*(5.0/1023.0*1000.0);
+  Serial.println(napiecie);
+  Serial.println(odczytanawartosc);
+  delay(1000);
+  
+}
